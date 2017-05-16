34ac118b2a71d5cca25dc2cc3fb5858d38c50069
EmKen <emily.kennedy_95@hotmail.co.uk>
2017-05-16 16:38:10 +0800

    Companies table created

--- db/migrate/20170516053330_create_users.rb
+++ db/migrate/20170516053330_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.timestamps null: false
+      t.belongs_to :company
       t.string  :first_name, null: false
       t.string  :last_name, null: false
       t.string  :job_title, null: false
@@ -11,6 +12,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string  :encrypted_password, limit: 128, null: false
       t.string  :confirmation_token, limit: 128
       t.string  :remember_token, limit: 128, null: false
+      t.string  :private_token
     end
 
     add_index :users, :email
