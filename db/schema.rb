e89de2d5c80dd93ae3763c854b127e6214a57e0e
EmKen <emily.kennedy_95@hotmail.co.uk>
2017-05-17 10:09:57 +0800

    Edited application layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516065211) do
+ActiveRecord::Schema.define(version: 20170516102441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,15 @@ ActiveRecord::Schema.define(version: 20170516065211) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "skills", force: :cascade do |t|
+    t.string "skill"
+    t.string "category"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                                 null: false
     t.datetime "updated_at",                                 null: false
+    t.integer  "company_id"
     t.string   "first_name",                                 null: false
     t.string   "last_name",                                  null: false
     t.string   "job_title",                                  null: false
@@ -34,6 +40,8 @@ ActiveRecord::Schema.define(version: 20170516065211) do
     t.string   "encrypted_password", limit: 128,             null: false
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128,             null: false
+    t.string   "private_token"
+    t.index ["company_id"], name: "index_users_on_company_id", using: :btree
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
   end
