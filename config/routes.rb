4b9a0d48a4ac204c272a4b569268be88e4a82ecc
Joseph Wong <josephwong@Joseph-Wongs-MacBook-Pro.local>
2017-05-18 17:54:34 +0800

    logout button, skills index page made

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root "sessions#new"
-  
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
   resources :users, controller: "users" do
     resource :password,
       controller: "clearance/passwords",
-      only: [:create, :edit, :update, ]
+      only: [:create, :edit, :update ]
   end
 
   get "/sign_in" => "sessions#new", as: "sign_in"
   delete "/sign_out" => "sessions#destroy", as: "sign_out"
   get "/sign_up" => "users#new", as: "sign_up"
 
-  resources :skills, only: [:index, :create]
+  resources :skills, only: [:index, :create, :show]
 
   constraints Clearance::Constraints::SignedIn.new do
    root to: "users#show", as: :signed_in_root
