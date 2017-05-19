d13a877c593a73c82f3e654f6de34a76d7ef5835
EmKen <emily.kennedy_95@hotmail.co.uk>
2017-05-19 16:26:54 +0800

    User can build squad

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,13 +19,19 @@ class ProjectsController < ApplicationController
 	def approve
 		@project = Project.find(params[:id])
 		@project.approved!
-		redirect_to "/"
+		@project.approved_or_refused_by = current_user
+		if @project.save
+			redirect_to "/"
+		end
 	end
 
 	def refuse
 		@project = Project.find(params[:id])
 		@project.refused!
-		redirect_to "/"
+		@project.approved_or_refused_by = current_user
+		if @project.save
+			redirect_to "/"
+		end
 	end
 
 	def build_squad
@@ -34,6 +40,14 @@ class ProjectsController < ApplicationController
 
 	def team_members
 		@project = Project.find(params[:id])
+		@project.skills.ids.each do |skill_id|
+			project_skill_id = ProjectSkill.find_by(skill_id: skill_id, project_id: @project.id).id
+			if params["#{skill_id}"]
+				params["#{skill_id}"].each do |user_id|
+					team_member = ProjectTeamMember.create(project_id: @project.id, user_id: user_id, project_skill_id: project_skill_id)
+				end
+			end
+		end
 		redirect_to @project
 	end
 
